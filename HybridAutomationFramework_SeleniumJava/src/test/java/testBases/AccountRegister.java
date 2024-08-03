99c0faba60693b75021f8a2e659e4241e1c85d79
MohanKumarR13 <svgvedha@gmail.com>
2024-08-04 00:21:10 +0530

    hybrid framework

--- HybridAutomationFramework_SeleniumJava/src/test/java/testBases/AccountRegister.java
+++ HybridAutomationFramework_SeleniumJava/src/test/java/testBases/AccountRegister.java
@@ -8,34 +8,29 @@ import pageObjects.HomePage;
 
 public class AccountRegister extends BaseClass {
 
-@Test
-public void verify_register() {
-	HomePage hp=new HomePage(driver);
-	hp.clickMyAccount();
-	hp.clickRegister();
-	
-	AccountRegisterationPage accountRegisterationPage=new AccountRegisterationPage(driver);
-	
-	accountRegisterationPage.setTxtFirstName(randomString().toUpperCase());
-	accountRegisterationPage.setTxtLastName(randomString().toUpperCase());
-	accountRegisterationPage.setTxtEmail(randomString()+"@gmail.com");
-	accountRegisterationPage.setTxtTelePhone(randomNumber());
-	
-	String passwd=randomAlphaNumeric();
-	accountRegisterationPage.setTxtPassword(passwd);
-	accountRegisterationPage.setTxtConfrimPassword(passwd);
-	
-	accountRegisterationPage.setCheckPolicy();
-	accountRegisterationPage.setBtnContinue();
-	
-	String confirmMsg=accountRegisterationPage.getConfirmationMessage();
-	Assert.assertEquals(confirmMsg, "Your Account Has Been Created!");
+	@Test
+	public void verify_register() {
+		HomePage hp = new HomePage(driver);
+		hp.clickMyAccount();
+		hp.clickRegister();
 
+		AccountRegisterationPage accountRegisterationPage = new AccountRegisterationPage(driver);
 
+		accountRegisterationPage.setTxtFirstName(randomString().toUpperCase());
+		accountRegisterationPage.setTxtLastName(randomString().toUpperCase());
+		accountRegisterationPage.setTxtEmail(randomString() + "@gmail.com");
+		accountRegisterationPage.setTxtTelePhone(randomNumber());
 
+		String passwd = randomAlphaNumeric();
+		accountRegisterationPage.setTxtPassword(passwd);
+		accountRegisterationPage.setTxtConfrimPassword(passwd);
 
+		accountRegisterationPage.setCheckPolicy();
+		accountRegisterationPage.setBtnContinue();
 
+		String confirmMsg = accountRegisterationPage.getConfirmationMessage();
+		Assert.assertEquals(confirmMsg, "Your Account Has Been Created!");
 
-}
+	}
 
 }
